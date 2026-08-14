72df4713c0e6335976e21152cd25c26c7c5ca737
윤강 <yg920@naver.com>
2026-08-14 16:37:15 +0900

    P_base_1

--- members/views.py
+++ members/views.py
@@ -1,25 +1,76 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
-# Create your views here.
-from django.shortcuts import render, redirect
 from .models import Member
-from .forms import MemberForm 
+from .forms import MemberForm
 
-# 1. 기존에 만들었던 목록 뷰
+
+# 1. 회원 목록
 def member_list(request):
     members = Member.objects.all()
-    return render(request, 'members/member_list.html', {'members': members})
 
-# 2. 🔥 새로 추가할 등록 뷰
+    return render(
+        request,
+        'members/member_list.html',
+        {'members': members}
+    )
+
+
+# 2. 회원 등록
 def member_create(request):
     if request.method == "POST":
-        # 사용자가 데이터를 입력하고 [저장]을 눌렀을 때
         form = MemberForm(request.POST)
+
         if form.is_valid():
-            form.save()  # DB에 실시간으로 저장!
-            return redirect('member_list')  # 저장 후 목록 화면으로 튕겨주기
+            form.save()
+            return redirect('member_list')
+
     else:
-        # 처음에 주소창 치고 들어왔을 때 (빈 입력창 보여주기)
         form = MemberForm()
-    
-    return render(request, 'members/member_create.html', {'form': form})
+
+    return render(
+        request,
+        'members/member_create.html',
+        {'form': form}
+    )
+
+
+# 3. 회원 수정
+def member_update(request, member_id):
+    member = get_object_or_404(
+        Member,
+        pk=member_id
+    )
+
+    if request.method == "POST":
+        form = MemberForm(
+            request.POST,
+            instance=member
+        )
+
+        if form.is_valid():
+            form.save()
+            return redirect('member_list')
+
+    else:
+        form = MemberForm(
+            instance=member
+        )
+
+    return render(
+        request,
+        'members/member_update.html',
+        {
+            'form': form,
+            'member': member
+        }
+    )
+
+
+# 4. 회원 삭제
+def member_delete(request, member_id):
+    member = get_object_or_404(Member, pk=member_id)
+
+    if request.method == "POST":
+        member.delete()
+
+    return redirect('member_list')
